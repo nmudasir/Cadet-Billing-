5cd313dd0c297830c5c310dc892da2acceae1309
N.Mudasir Ahmed <n03347124766@gmail.com>
2026-04-21 16:40:18 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+var CACHE = 'cadetstat-v1';
+var FILES = ['/', '/index.html'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(FILES);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      return r || fetch(e.request).then(function(res) {
+        return caches.open(CACHE).then(function(cache) {
+          cache.put(e.request, res.clone());
+          return res;
+        });
+      }).catch(function() {
+        return caches.match('/index.html');
+      });
+    })
+  );
+});
